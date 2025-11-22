abdc1feb07721e41294aecd0aa0a956dd450f7aa
Julia-proj <juleea0@gmail.com>
2025-11-23 00:46:57 +0100

    Update hero background image path

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import heroBg from "images/foto11.PNG";
+import heroBg from "@/assets/hero-bg.jpg";
 
 const Hero = () => {
   const scrollToSection = (id: string) => {
