518ca8d1cdcc481c24ae5de66a1f1fe14b9e26ff
Julia-proj <juleea0@gmail.com>
2025-11-23 00:44:04 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import heroBg from "@/assets/hero-bg.jpg";
+import heroBg from "public/images/foto11.PNG";
 
 const Hero = () => {
   const scrollToSection = (id: string) => {
